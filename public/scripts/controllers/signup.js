26ad966afb928fd227035f82e471d53b2a0af72e
Rahim <raskarzadeh1@my.bcit.ca>
2020-04-30 13:57:00 -0700

    added fb to signup

--- public/scripts/controllers/signup.js
+++ public/scripts/controllers/signup.js
@@ -3,6 +3,8 @@
  */
 function addOnClickHandlers() {
     document.getElementById("signinbutton").onclick = goToLoginPage;
+    document.getElementById("signupbutton").onclick = register;
+
 }
 addOnClickHandlers();
 
@@ -18,32 +20,23 @@ function goToLoginPage() {
  * @desc register user to Firebase.
  */
 function register() {
-    let pw = document.getElementById("pwInput").value;
-    let pwCon = document.getElementById("pwInputCon").value;
-    let name = document.getElementById("nameInput").value;
-    let email = document.getElementById("emailInput").value;
+    let pw = document.getElementById("inputPassword").value;
+    let name = document.getElementById("inputName").value;
+    let email = document.getElementById("inputEmail").value;
 
-    if (pw != pwCon) {
-        window.alert("Passwords do not match");
-        return;
-
-    } else {
-        firebase.auth().createUserWithEmailAndPassword(email, pw).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            window.alert(errorMessage);
-            return false;
+    firebase.auth().createUserWithEmailAndPassword(email, pw).catch(function(error) {
+        // Handle Errors here.
+        var errorMessage = error.message;
+        window.alert(errorMessage);
+        return false;
+    }).then(function() {
+        let user = firebase.auth().currentUser;
+        db.collection("users").doc(user.uid).set({
+            name: name,
+            email: email,
+            // dateOfCreation : event.toString()
         }).then(function() {
-            let user = firebase.auth().currentUser;
-            let event = new Date();
-            db.collection("users").doc(user.uid).set({
-                name: name,
-                email: email,
-                // dateOfCreation : event.toString()
-            }).then(function() {
-                window.location = "index.html";
-            })
-        });
-    }
+            window.location = "index.html";
+        })
+    });
 }

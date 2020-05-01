5232b28ba84a4469e173a308cdfd46f798273f67
Rahim <raskarzadeh1@my.bcit.ca>
2020-05-01 02:36:33 -0700

    added spinner on the button to prevent double clk

--- public/scripts/controllers/signup.js
+++ public/scripts/controllers/signup.js
@@ -20,8 +20,9 @@ function goToLoginPage() {
  * @desc register user to Firebase.
  */
 function register() {
+    document.getElementById("signupbutton").disabled = true;
+    document.getElementById("signupbutton").innerHTML = '<span class="spinner-border spinner-border-sm mr-2 disabled" role="status" aria-hidden="true"></span>Loading...';
 
-    console.log("register");
     let pw = document.getElementById("inputPassword").value;
     let name = document.getElementById("inputName").value;
     let email = document.getElementById("inputEmail").value;
@@ -41,11 +42,17 @@ function register() {
         }).catch(function(error) {
             // An error happened while adding data to firestore. 
             window.alert(error.message);
+            document.getElementById("signupbutton").disabled = false;
+            document.getElementById("signupbutton").innerHTML = "Sign up"
+
         });
 
     }).catch(function(error) {
         // Handle Errors here.
         window.alert(error.message);
+        document.getElementById("signupbutton").disabled = false;
+        document.getElementById("signupbutton").innerHTML = "Sign up"
+
     });
 
 

296ee22d13bcde8728c4f2eefc22560797d9c0fa
Rahim <raskarzadeh1@my.bcit.ca>
2020-05-01 00:50:59 -0700

    updated js to connect to fb and read data

--- public/scripts/controllers/editprofile.js
+++ public/scripts/controllers/editprofile.js
@@ -1,23 +1,84 @@
 /**
- * @desc Check user state.
+ * @desc logs out current user
  */
-firebase.auth().onAuthStateChanged(function(user) {
-    loadUserInfo(user);
-});
+function logout() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        window.location = "login.html";
+    }).catch(function(error) {
+        window.alert(error);
+    })
+}
+
+/**
+ * @desc checks if user is logged in and if not kicks them out.
+ */
+function checkCred() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            window.location = "login.html";
+        } else {
+            loadUserInfo();
+        }
+    });
+}
+
+checkCred();
 
 /**
  * @desc loads user information from Firebase
- * @param user the current logged in user.
  */
-function loadUserInfo(user) {
+function loadUserInfo() {
     let name = document.getElementById("username")
-    let email = document.getElementById("validationCustomEmail");
-    let docRef = db.collection("users").doc(user.uid);
+    let inputname = document.getElementById("inputName")
+    let inputemail = document.getElementById("inputEmail");
 
-    docRef.get().then(function(doc) {
-        name.innerHTML = doc.data().name;
-        email.value = doc.data().email;
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
+    let user = firebase.auth().currentUser;
+    if (user != null) {
+        user.providerData.forEach(function(profile) {
+
+            name.innerHTML = profile.displayName;
+            inputname.value = profile.displayName;
+            inputemail.value = profile.email;
+
+        });
+    }
+
+}
+
+
+
+
+/**
+ * @desc update user info. 
+ */
+function updateProfile() {
+    let inputname = document.getElementById("inputName")
+    let inputemail = document.getElementById("inputEmail");
+
+    let user = firebase.auth().currentUser;
+    if (user != null) {
+        user.updateProfile({
+            displayName: inputname.value
+        }).then(function() {
+            user.updateEmail(inputemail).then(function() {
+                // update firstore DB 
+                db.collection("users").doc(user.uid).set({
+                    name: inputname.value,
+                    email: inputemail.value,
+                }).then(function() {
+                    window.location.reload();
+                }).catch(function(error) {
+                    //error updating database
+                    window.alert(error);
+                });
+            }).catch(function(error) {
+                // error updating email
+                window.alert(error);
+            });
+        }).catch(function(error) {
+            // error updating display name
+        });
+    }
+
 }

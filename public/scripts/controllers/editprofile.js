c8a9f61540079e94ce4b324ea63f5ee6b2071018
Kyle Herrmann <swisskyle107@gmail.com>
2020-05-02 13:57:17 -0700

    Merging

---
+++ public/scripts/controllers/editprofile.js
@@ -0,0 +1,140 @@
+let credential;
+
+/**
+ * @desc adds onclick to buttons
+ */
+function init() {
+    document.getElementById("savechanges").onclick = updateProfile;
+
+    // Prompt the user to re-provide their sign-in credentials
+
+    bootbox.prompt({
+        title: "Please enter your password to make changes to your profile.",
+        inputType: 'password',
+        callback: function(result) {
+
+            if (result === null) {
+                window.location.assign("index.html");
+            } else {
+                let user = firebase.auth().currentUser;
+                credential = firebase.auth.EmailAuthProvider.credential(
+                    user.email,
+                    result
+                );
+            }
+
+
+        }
+    });
+
+}
+
+init();
+
+/**
+ * @desc logs out current user
+ */
+function logout() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        window.location = "login.html";
+    }).catch(function(error) {
+        window.alert(error);
+    })
+}
+
+/**
+ * @desc checks if user is logged in and if not kicks them out.
+ */
+function checkCred() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            window.location = "login.html";
+        } else {
+            loadUserInfo();
+        }
+    });
+}
+
+checkCred();
+
+/**
+ * @desc loads user information from Firebase
+ */
+function loadUserInfo() {
+    let name = document.getElementById("username")
+    let inputname = document.getElementById("inputName")
+    let inputemail = document.getElementById("inputEmail");
+
+    let user = firebase.auth().currentUser;
+    if (user != null) {
+        user.providerData.forEach(function(profile) {
+
+            name.innerHTML = profile.displayName;
+            inputname.value = profile.displayName;
+            inputemail.value = profile.email;
+
+        });
+    }
+
+}
+
+
+/**
+ * @desc update user info. 
+ */
+function updateProfile() {
+    document.getElementById("savechanges").disabled = true;
+    document.getElementById("savechanges").innerHTML = '<span class="spinner-border spinner-border-sm mr-2 disabled" role="status" aria-hidden="true"></span>Loading...';
+
+    let inputname = document.getElementById("inputName")
+    let inputemail = document.getElementById("inputEmail");
+    console.log();
+
+    let user = firebase.auth().currentUser;
+    if (user != null) {
+        user.updateProfile({
+            displayName: inputname.value
+        }).then(function() {
+            user.reauthenticateWithCredential(credential).then(function() {
+                // User re-authenticated.
+                user.updateEmail(inputemail.value).then(function() {
+                    // update firstore DB 
+                    db.collection("users").doc(user.uid).update({
+                        name: inputname.value,
+                        email: inputemail.value,
+                    }).then(function() {
+                        window.alert("Succesfully updated changes!")
+                        window.location.assign("dashboard.html");
+                    }).catch(function(error) {
+                        //error updating database
+                        window.alert(error);
+                        document.getElementById("savechanges").disabled = false;
+                        document.getElementById("savechanges").innerHTML = "Save Changes"
+
+                    });
+                }).catch(function(error) {
+                    // error updating email
+                    window.alert(error.message);
+                    document.getElementById("savechanges").disabled = false;
+                    document.getElementById("savechanges").innerHTML = "Save Changes"
+
+                });
+            }).catch(function(error) {
+                // An error happened.
+                window.alert(error.message);
+                document.getElementById("savechanges").disabled = false;
+                document.getElementById("savechanges").innerHTML = "Save Changes"
+
+                init();
+
+            });
+        }).catch(function(error) {
+            // error updating display name
+            document.getElementById("savechanges").disabled = false;
+            document.getElementById("savechanges").innerHTML = "Save Changes"
+
+        });
+    }
+
+}
---
+++ public/scripts/controllers/login.js
@@ -0,0 +1,54 @@
+/**
+ * @desc adds the onclick handlers to buttons
+ */
+function addOnClickHandlers() {
+    document.getElementById("signupbutton").onclick = goToSignUpPage;
+    document.getElementById("signinbutton").onclick = login;
+
+
+}
+addOnClickHandlers();
+
+/**
+ * @desc redirect to signup page 
+ */
+function goToSignUpPage() {
+    window.location.assign("signup.html");
+}
+
+
+/**
+ * @desc login user and redirect to index page 
+ */
+function login() {
+    document.getElementById("signinbutton").disabled = true;
+    document.getElementById("signinbutton").innerHTML = '<span class="spinner-border spinner-border-sm mr-2 disabled" role="status" aria-hidden="true"></span>Loading...';
+
+    let email = document.getElementById("inputEmail").value;
+    let password = document.getElementById("inputPassword").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+
+        window.location = "dashboard.html";
+
+    }).catch(function(error) {
+        switch (error.code) {
+            case "auth/user-not-found":
+                window.alert("Email address not found. Please try again!");
+                document.getElementById("signinbutton").disabled = false;
+                document.getElementById("signinbutton").innerHTML = "Sign in"
+
+                break;
+            case "auth/wrong-password":
+                window.alert("Incorrect Password. Please try again!");
+                document.getElementById("signinbutton").disabled = false;
+                document.getElementById("signinbutton").innerHTML = "Sign in"
+                break;
+            default:
+                var errorMessage = error.message;
+                window.alert(errorMessage);
+                document.getElementById("signinbutton").disabled = false;
+                document.getElementById("signinbutton").innerHTML = "Sign in"
+        }
+    });
+}

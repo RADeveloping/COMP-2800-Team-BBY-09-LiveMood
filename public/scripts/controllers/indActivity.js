142ab3ce56fced68b8a400447b32308ac24900c1
Laura <legillespie5@hotmail.com>
2020-05-06 18:35:06 -0700

    it works, not pretty tho

--- public/scripts/controllers/indActivity.js
+++ public/scripts/controllers/indActivity.js
@@ -1,8 +1,60 @@
-function getActivityResult() {
-	db.collection('individualActivities').get().then((snapshot) => {
+const moodScore = 1;
+
+// function logout() {
+// 	firebase
+// 		.auth()
+// 		.signOut()
+// 		.then(function() {
+// 			// Sign-out successful.
+// 			window.location = 'login.html';
+// 		})
+// 		.catch(function(error) {
+// 			window.alert(error);
+// 		});
+// }
+
+// function checkCred() {
+// 	firebase.auth().onAuthStateChanged(function(user) {
+// 		if (!user) {
+// 			window.location = 'login.html';
+// 		} else {
+// 			init();
+// 		}
+// 	});
+// }
+
+// checkCred();
+
+// let user;
+
+// function init() {
+// 	user = firebase.auth().currentUser;
+// 	indActivity();
+// }
+const actSuggestion = document.querySelector('.suggestion');
+
+function renderSuggestion(doc) {
+	let activity = document.createElement('div');
+	let activityName = document.createElement('h2');
+	let activityLink = document.createElement('a');
+	activity.setAttribute('data-id', doc.id);
+	activityName.textContent = doc.data().name;
+	activityLink.textContent = doc.data().link;
+
+	activity.appendChild(activityName);
+	activity.appendChild(activityLink);
+
+	actSuggestion.appendChild(activity);
+}
+
+function indActivity() {
+	db.collection('individualActivities ').get().then((snapshot) => {
 		snapshot.docs.forEach((doc) => {
 			console.log(doc.data());
+			if (doc.data().rating == moodScore) {
+				renderSuggestion(doc);
+			}
 		});
 	});
 }
-getActivityResult();
+indActivity();

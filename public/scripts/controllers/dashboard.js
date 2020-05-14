f5ddce887439010c78578a316cf689eb79a31fa5
Kyle Herrmann <swisskyle107@gmail.com>
2020-05-14 12:48:37 -0700

    More graph update

--- public/scripts/controllers/dashboard.js
+++ public/scripts/controllers/dashboard.js
@@ -80,8 +80,8 @@ function setGraph(month, data) {
         maintainAspectRatio: true,
         layout: {
           padding: {
-            left: 5,
-            right: 5,
+            left: 0,
+            right: 0,
             top: 0,
             bottom: 0
           }

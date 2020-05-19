38f84c21dfca384555c30ca383e4a9c869b255ca
Rahim <raskarzadeh1@my.bcit.ca>
2020-05-19 16:10:21 -0700

    added latest covid stats for canada

--- public/scripts/controllers/dashboard.js
+++ public/scripts/controllers/dashboard.js
@@ -8,135 +8,131 @@ function goToProfilePage() {
 /* Handle month select */
 function setGraph(month) {
 
-  // Display heading
-  document.getElementById("chartText").innerText =
-    "Personal Chart for " + month;
+    // Display heading
+    document.getElementById("chartText").innerText =
+        "Personal Chart for " + month;
 
-  // Display graph
-  var colors = [ // Color
-    "#007bff",
-    "#28a745",
-    "#333333",
-    "#c3e6cb",
-    "#dc3545",
-    "#6c757d",
-  ];
+    // Display graph
+    var colors = [ // Color
+        "#007bff",
+        "#28a745",
+        "#333333",
+        "#c3e6cb",
+        "#dc3545",
+        "#6c757d",
+    ];
 
-  var chLine = document.getElementById("chLine"); // Type of chart
-  var chartData = {
-    labels: [ // Labels
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "11",
-      "12",
-      "13",
-      "14",
-      "15",
-      "16",
-      "17",
-      "18",
-      "19",
-      "20",
-      "21",
-      "22",
-      "23",
-      "24",
-      "25",
-      "26",
-      "27",
-      "28",
-      "29",
-      "30",
-    ],
-    datasets: [
-      {
-        data: getMonthScore(month), // Get user data!
-        backgroundColor: "transparent",
-        borderColor: colors[0],
-        borderWidth: 4,
-        pointBackgroundColor: colors[0],
-      },
-    ],
-  };
+    var chLine = document.getElementById("chLine"); // Type of chart
+    var chartData = {
+        labels: [ // Labels
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+        ],
+        datasets: [{
+            data: getMonthScore(month), // Get user data!
+            backgroundColor: "transparent",
+            borderColor: colors[0],
+            borderWidth: 4,
+            pointBackgroundColor: colors[0],
+        }, ],
+    };
 
-  if (chLine) {
-    new Chart(chLine, {
-      type: "line",
-      data: chartData,
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: false,
-              },
+    if (chLine) {
+        new Chart(chLine, {
+            type: "line",
+            data: chartData,
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: false,
+                        },
+                    }, ],
+                },
+                legend: {
+                    display: false,
+                },
             },
-          ],
-        },
-        legend: {
-          display: false,
-        },
-      },
-    });
-  }
+        });
+    }
 }
 
 /**
  * Retrieve and display user score
  */
 function getMonthScore(month) {
-  let monthScore = [];
-  let year = new Date().getFullYear();
-  let daysInMonth = new Date(year, month, 0).getDate();
-  let formatMonth;
+    let monthScore = [];
+    let year = new Date().getFullYear();
+    let daysInMonth = new Date(year, month, 0).getDate();
+    let formatMonth;
 
-  // Format month to 2 digit
-  if (month < 10) {
-    formatMonth = "0" + month;
-  } else {
-    formatMonth = month;
-  }
-
-  //Iterate over days in a month
-  for (let dd = 1; dd <= daysInMonth; dd++) {
-    // Format Day to 2 digit
-    let day;
-    if (dd < 10) {
-      day = "0" + dd;
+    // Format month to 2 digit
+    if (month < 10) {
+        formatMonth = "0" + month;
     } else {
-      day = dd;
+        formatMonth = month;
     }
 
-    // Form survey ID and reference
-    let scoreId = "" + year + formatMonth + day + userId.substring(0, 13);
-    let userDayScoreRef = db.collection("surveyTaken").doc(scoreId);
-
-    // Add to array
-    userDayScoreRef
-      .get()
-      .then(function (doc) {
-        if (doc.exists) {
-          // Get past score
-          monthScore.push(doc.data()["score"]);
+    //Iterate over days in a month
+    for (let dd = 1; dd <= daysInMonth; dd++) {
+        // Format Day to 2 digit
+        let day;
+        if (dd < 10) {
+            day = "0" + dd;
         } else {
-          // No score for the day
-          monthScore.push(null);
+            day = dd;
         }
-      })
-      .catch(function (error) {
-        console.log("Error getting document:", error);
-      });
-  }
 
-  return monthScore;
+        // Form survey ID and reference
+        let scoreId = "" + year + formatMonth + day + userId.substring(0, 13);
+        let userDayScoreRef = db.collection("surveyTaken").doc(scoreId);
+
+        // Add to array
+        userDayScoreRef
+            .get()
+            .then(function(doc) {
+                if (doc.exists) {
+                    // Get past score
+                    monthScore.push(doc.data()["score"]);
+                } else {
+                    // No score for the day
+                    monthScore.push(null);
+                }
+            })
+            .catch(function(error) {
+                console.log("Error getting document:", error);
+            });
+    }
+
+    return monthScore;
 }
 
 function logout() {
@@ -176,3 +172,18 @@ function setUserName() {
     document.getElementById("username").innerText = user.displayName;
 }
 
+$.getJSON('https://api.covid19api.com/summary', function(data) {
+        //covidinfo
+        if (data) {
+            let totalConfirmed = data["Countries"][30]["TotalConfirmed"];
+            let TotalRecovered = data["Countries"][30]["TotalRecovered"];
+
+            let TotalDeaths = data["Countries"][30]["TotalDeaths"];
+            let string = 'As of date there has been a total of <strong>' + totalConfirmed + ' </strong>' + 'confirmed cases of COVID-19 and a total of <strong>' + TotalRecovered + '</strong> confirmed recoveries in Canada. <small>Data provided by: covid19api.com</small>';
+
+            document.getElementById("covidinfo").innerHTML = string;
+        }
+    })
+    .fail(function() {
+        document.getElementById("coronaBanner").classList.replace("d-block", "d-none");
+    });

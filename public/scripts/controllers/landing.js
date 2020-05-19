0568513f6045e2db9ede12fb6199afd327de9083
JasonPan <jpan569@gmail.com>
2020-05-19 15:10:40 -0700

    Styling, different buttons for user and visitor.

---
+++ public/scripts/controllers/landing.js
@@ -0,0 +1,12 @@
+function checkCred() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            $("#loginButton").css("display", "list-item");
+            $("#registerButton").css("display", "list-item");
+        }else{
+            $("#dashboardButton").css("display", "list-item");
+        }
+    });
+}
+
+checkCred();
